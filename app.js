79c561d3d5eb16e33a8c1f10c9c287023c94f878
Ben W <mrbrwells@gmail.com>
2015-04-19 10:19:51 -0400

    added new styles and services section

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
   methodOverride = require('jade'),
-  mongoose = require('mongoose');
+  // mongoose = require('mongoose'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
 
 //require routes file
 require('./app/routes.js')(app);
 
 //jade config
-app.set('views', __dirname + '/app/views')
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 
 // app.use(express.static(__dirname + '/'));
@@ -23,7 +26,33 @@ require('./app/routes')(app);
 // });
 
 // app.use(app.router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
+
+// passport config
+// var Account = require('./app/models/account');
+// passport.use(new LocalStrategy(Account.authenticate()));
+// passport.serializeUser(Account.serializeUser());
+// passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+// mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 //server and port
 var port = process.env.PORT || 3000;
