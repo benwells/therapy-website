fe06153225248dd3b3673e80b4a4405a60eae30c
Ben W <mrbrwells@gmail.com>
2015-04-19 10:30:51 -0400

    updated port and ip for rhc

--- app.js
+++ app.js
@@ -55,6 +55,13 @@ app.use(function(req, res, next) {
 });
 
 //server and port
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("starting server on port", port);
+// var port = process.env.PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port );
+});
+
+app.listen(server_port);
+console.log("starting server on port", server_port);
